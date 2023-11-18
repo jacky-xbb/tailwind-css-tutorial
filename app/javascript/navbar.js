68f48695873b6bd80ab02103b8b0f963fdd04c93
jacky-xbb <jacky-xbb@gmail.com>
2023-11-18 10:35:15 +0800

    first commit

---
+++ app/javascript/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.getElementById('burger');
+  const menu = document.getElementById('menu');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
+
